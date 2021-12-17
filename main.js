f550034858cb249be94eb5040d9b6dda32aaa1b8
wuwei <weiwu@yitong.com.cn>
2021-12-17 10:41:44 +0800

    init

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1080,
+    height: 768,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
